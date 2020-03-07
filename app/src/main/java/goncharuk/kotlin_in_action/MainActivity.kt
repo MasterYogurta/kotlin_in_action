5710fdba4bf78a5f95849f2d514ab216ca25ee09
Pavel <pavlusius@gmail.com>
2020-03-07 18:04:28 +0300

    + android project init

---
+++ app/src/main/java/goncharuk/kotlin_in_action/MainActivity.kt
@@ -0,0 +1,12 @@
+package goncharuk.kotlin_in_action
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
